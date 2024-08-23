044a7f971579f731e191b738a9db887d5beb5cab
Connor Page <george@connorpage.com>
2024-08-23 13:28:09 -0500

    COmpletes lab

--- server/app.py
+++ server/app.py
@@ -44,9 +44,22 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get_or_404(id)
+        return jsonify(plant.to_dict())
 
+
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+        plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+        db.session.commit()
+        return jsonify(plant.to_dict())
+    
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
